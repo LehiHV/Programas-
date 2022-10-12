3259bc57fd612db66390786a5ef3c7089a7c5f76
HERNANDEZ VELASCO VELASCO <113217024+LehiHV@users.noreply.github.com>
2022-10-12 18:57:49 +0000

    add Practica_Pilas for the activitie in DataSctructure

---
+++ 2p/Practica_Pilas.js
@@ -0,0 +1,84 @@
+function pila(maximo) {
+  this.Tamaño_Pila = []
+  this.max = maximo
+  this.llena = llena
+  this.vacia = vacia
+  this.entrada = entrada
+  this.salida = salida
+  this.mostrar = mostrar
+  this.busqueda = busqueda
+  this.peek = peek
+}
+function entrada(element) {
+  if (this.llena()) {
+    console.log("Tu pila esta llena, debes eliminar elementos")
+  } else {
+    this.Tamaño_Pila.unshift(element)
+  }
+}
+
+function salida() {
+  if (this.vacia()) {
+    console.log("Tu pila esta vacia, debes introducit elementos")
+  }
+  else {
+    this.Tamaño_Pila.shift()
+  }
+}
+
+function llena() {
+  if (this.Tamaño_Pila.length === this.max)
+    return true
+  else
+    return false
+}
+
+function vacia() {
+  if (this.Tamaño_Pila.length === 0)
+    return true
+  else
+    return false
+}
+
+function mostrar() {
+  let show = ""
+  for (let i = 0; i < this.Tamaño_Pila.length; ++i) {
+    show += this.Tamaño_Pila[i] + "\n"
+  }
+  return show
+}
+
+function busqueda(x) {
+  let enc;
+  for (let i = 0; i < this.Tamaño_Pila.length; i++) {
+    if (this.Tamaño_Pila[i] == x) {
+      enc = this.Tamaño_Pila[i]
+    }
+  }
+  if (enc != "") {
+    return "Se encontro el valor: " + x + " en la posicion: " + (enc + 1)
+  } else {
+    return "No se encontro el valor: " + x + ", Su posicion es -1"
+  }
+}
+
+function peek() {
+  return this.Tamaño_Pila[this.max - 1]
+}
+
+function ord_pila(x) {
+  let p = new pila(x);
+  let s = new pila(x);
+  while(i==x){
+    n=Math.floor(Math.random() * (10 - 1 + 1))
+    if(n>p.peek()){
+      p.entrada(n)
+    }else{
+      s.entrada(p.salida())
+    }
+  }
+  console.log(p.mostrar());
+}
+ord_pila(10)
+console.log("Otra pila")
+ord_pila(100)
